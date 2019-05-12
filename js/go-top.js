12189bf77a1343156aa8f2a4f7c8f209c4ff35ca
roothash <42235230+Roothash41@users.noreply.github.com>
2019-05-13 03:06:25 +0800

    Update go-top.js

--- js/go-top.js
+++ js/go-top.js
@@ -6,6 +6,6 @@
             $('div.go-top').hide();
     });
     $('div.go-top').click(function() {
-        $('html, body').animate({scrollTop: 0}, 100);
+        $('html, body').animate({scrollTop: 0}, 1000);
     });
 });
